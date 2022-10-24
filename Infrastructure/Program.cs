f2c71bbf9493ebcea7d27507cf509f044e2d912a
Museberg <Rasmus@falk-jensen.dk>
2022-10-24 12:00:43 +0200

    Added automatic migration

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -40,6 +40,14 @@ SetupSwagger();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<MeFitDbContext>();    
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
