419a7afd8c72909e6aa4c22069691095924a14ae
Museberg <Rasmus@falk-jensen.dk>
2022-10-24 14:25:32 +0200

    fix: new port and developer mode always enabled

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -64,7 +64,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || true)
 {
     app.UseDeveloperExceptionPage();
     app.SeedDatabase();
