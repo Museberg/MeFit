b12ad881e7f18724783979614adbf9eaae1e3a64
Museberg <Rasmus@falk-jensen.dk>
2022-10-26 08:22:32 +0200

    fix: Now only generates 10 fake profiles

--- Infrastructure/Data/DbInitializer.cs
+++ Infrastructure/Data/DbInitializer.cs
@@ -6,7 +6,7 @@ public class DbInitializer
 {
     public static void Initialize(MeFitDbContext context)
     {
-        const int profilesToFake = 100;
+        const int profilesToFake = 10;
         context.Database.EnsureCreated();
 
         if (context.Profiles.Count() < profilesToFake)
