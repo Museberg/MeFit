d61156529b34b09425bb38de3a3dd320cc14be81
Museberg <Rasmus@falk-jensen.dk>
2022-10-30 17:44:03 +0100

    fix: goat automapper date parsing

--- Infrastructure/Profiles/GoalProfile.cs
+++ Infrastructure/Profiles/GoalProfile.cs
@@ -8,7 +8,7 @@ namespace Infrastructure.Profiles
     {
         public GoalProfile()
         {
-            var format = "MM/DD/YYYY";   
+            var format = "dd/DD/yyyy";   
             
             CreateMap<Goal, GoalReadDTO>()
                 .ForMember(dest => dest.StartingDate, opt => opt
