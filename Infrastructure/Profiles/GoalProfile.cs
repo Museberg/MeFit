0a05fb48d0f7b411c85b160e2b701d0825ba7378
Nicolai <nicolai@enggren.dk>
2022-10-30 19:11:59 +0100

    fix: revert back changes in goal

--- Infrastructure/Profiles/GoalProfile.cs
+++ Infrastructure/Profiles/GoalProfile.cs
@@ -8,10 +8,18 @@ namespace Infrastructure.Profiles
     {
         public GoalProfile()
         {
-            var format = "dd/mm/yyyy";
-
-            CreateMap<Goal, GoalReadDTO>();
-            CreateMap<GoalCreateDTO, Goal>();
+            var format = "dd/mm/yyyy";   
+            
+            CreateMap<Goal, GoalReadDTO>()
+                .ForMember(dest => dest.StartingDate, opt => opt
+                .MapFrom(src => src.StartingDate.ToString()))
+                .ForMember(dest => dest.EndDate, opt => opt
+                .MapFrom(src => src.EndDate.ToString()));
+            CreateMap<GoalCreateDTO, Goal>()
+                .ForMember(dest => dest.StartingDate, opt => opt
+                .MapFrom(src => DateOnly.ParseExact(src.StartingDate, format)))
+                .ForMember(dest => dest.EndDate, opt => opt
+                .MapFrom(src => DateOnly.ParseExact(src.EndDate, format)));
             CreateMap<GoalEditDTO, Goal>();
         }
     }
